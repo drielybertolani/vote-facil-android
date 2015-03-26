7b36e614e32416bdc9d430ef4a0ec379b6e6c1b9
Driely Bertolani <driely.vix@gmail.com>
2015-03-26 08:00:43 -0300

    Initial Import

---
+++ VotaFacil/src/br/com/cesan/votafacil/MainActivity.java
@@ -0,0 +1,36 @@
+package br.com.cesan.votafacil;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+	private WebView webView;
+
+	@SuppressLint({ "SetJavaScriptEnabled", "NewApi" })
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		WebChromeClient chromeWebClient = new WebChromeClient() {
+			@Override
+			public boolean onJsAlert(WebView view, String url, String message,
+					JsResult result) {
+				return super.onJsAlert(view, url, message, result);
+			}
+		};
+		
+		webView = (WebView) findViewById(R.id.webView);
+		webView.setWebChromeClient(chromeWebClient);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setAllowContentAccess(true);
+		webView.getSettings().setDomStorageEnabled(true);
+		webView.getSettings().setDatabaseEnabled(true);
+		webView.loadUrl("http://drielybertolani.github.io/votafacil/home.html");
+	}
+}
