1aae6a2417735dd66bdca30c38578bccb7d0a1f5
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2015-03-30 16:10:50 -0300

    Identificado problemas com a lib do parsee. Removi o recurso para que o app seja publicado sem problemas.

--- VotaFacil/src/br/com/cesan/votafacil/VotaFacilApplication.java
+++ VotaFacil/src/br/com/cesan/votafacil/VotaFacilApplication.java
@@ -1,8 +1,5 @@
 package br.com.cesan.votafacil;
 
-import com.parse.Parse;
-import com.parse.ParseACL;
-
 import android.app.Application;
 
 public class VotaFacilApplication extends Application {
@@ -10,10 +7,10 @@ public class VotaFacilApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		
-		Parse.initialize(this, "AMWPVkiXCTh491UdP5PU5qP4qbRkuFnr3wQYwkq2", "wpz9034zJoF6avWKTvRk6jSqTN2PHoZC3LIrF8Rt");
-		
+		//Parse.initialize(this, "AMWPVkiXCTh491UdP5PU5qP4qbRkuFnr3wQYwkq2", "wpz9034zJoF6avWKTvRk6jSqTN2PHoZC3LIrF8Rt");
+		/*
 		ParseACL defaultACL = new ParseACL();
 		defaultACL.setPublicReadAccess(true);
-		ParseACL.setDefaultACL(defaultACL, true);
+		ParseACL.setDefaultACL(defaultACL, true);*/
 	}
 }
